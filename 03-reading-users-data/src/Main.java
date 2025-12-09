4c825b50d4bad33a4685a22d143a749726e82065
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2025-12-09 02:18:04 +0200

    Usage of scanner class to read user input.

--- 03-reading-users-data/src/Main.java
+++ 03-reading-users-data/src/Main.java
@@ -1,18 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         int currentYear = 2025;
-        String usersDateOfBirth = "1996";
 
-        int dateOfBirth = Integer.parseInt(usersDateOfBirth);
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
+        }
+    }
 
-        System.out.println("Age = " + (currentYear - dateOfBirth));
+    public static String getInputFromConsole(int currentYear) {
 
-        String usersAgeWithPartialYear = "28.5";
-        double ageWithPartialYear = Double.parseDouble(usersAgeWithPartialYear);
-        System.out.println("The user says he's " + ageWithPartialYear);
+        String name = System.console().readLine("Hi, What's your name? ");
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
 
+        String dateOfBirth = System.console().readLine("What year were you born? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
 
-        //soon
+        return "So you are " + age + " years old";
+    }
+
+    public static String getInputFromScanner(int currentYear) {
+
+        Scanner scanner = new Scanner(System.in);
+
+//        String name = System.console().readLine("Hi, What's your name? ");
+        System.out.println("Hi, what's your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
+
+//        String dateOfBirth = System.console().readLine("What year were you born? ");
+        System.out.println("What year were you born? ");
+        String dateOfBirth = scanner.nextLine();
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are " + age + " years old";
     }
 }
