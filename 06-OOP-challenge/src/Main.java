8661349fab6e8889272ce5277aaac400a0a9b057
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2025-12-17 03:09:58 +0200

    Add constructors and setters

--- 06-OOP-challenge/src/Main.java
+++ 06-OOP-challenge/src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        Account georgesAccount = new Account();
-        georgesAccount.setNumber("29391");
-        georgesAccount.setBalance(3000);
-        georgesAccount.setCustomerName("George Owen");
-        georgesAccount.setCustomerEmail("george@owen.com");
-        georgesAccount.setCustomerPhone("(082)145-5912");
+        Account georgesAccount = new Account("32141", 2000, "George Owen",
+                "george@owen.com", "(082)145-5912");
+
+        System.out.println(georgesAccount.getNumber());
+        System.out.println(georgesAccount.getBalance());
+
+//        georgesAccount.setNumber("29391");
+//        georgesAccount.setBalance(3000);
+//        georgesAccount.setCustomerName("George Owen");
+//        georgesAccount.setCustomerEmail("george@owen.com");
+//        georgesAccount.setCustomerPhone("(082)145-5912");
+
         georgesAccount.withdrawFunds(100);
         georgesAccount.depositFunds(500);
         georgesAccount.withdrawFunds(200);
