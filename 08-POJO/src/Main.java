fb5584ebe79aa11cb561f47c2813c329021fec17
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2025-12-25 05:05:30 +0200

    Introduce Record class

--- 08-POJO/src/Main.java
+++ 08-POJO/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {
-            Student s = new Student("S23101" + i,
+            LPAStudent s = new LPAStudent("S23101" + i,
                     switch (i) {
                         case 1 -> "Mary";
                         case 2 -> "Carol";
@@ -16,5 +16,23 @@ public class Main {
             );
             System.out.println(s);
         }
+        Student student = new Student("S2845025", "George",
+                "01/10/1990", "Java Course");
+
+        LPAStudent LPAstudent = new LPAStudent("S82910", "Anna",
+                "01/02/1994", "Java Course");
+
+        System.out.println(student);
+        System.out.println(LPAstudent);
+
+        student.setClassList(student.getClassList() + ", Java OCP exam 982");
+//        LPAstudent.setClassList(LPAstudent.classList() + ", Java OCP exam 982"); Doesn't work! Records are IMMUTABLE
+        
+        System.out.println(student.getName() + " is taking " +
+                student.getClassList());
+
+        System.out.println(LPAstudent.name() + " is taking " +
+                LPAstudent.classList());
+
     }
 }
