bca3d66e27d4337ec4d0905ec9e75a5a81a9ba9e
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2026-01-07 18:43:55 +0200

    feat: add polymorphism example with method overriding fix: correct return types in overridden methods refactor: clean up vehicle class hierarchy

---
+++ 22-polymorphism-car-challenge/src/Mitsubishi.java
@@ -0,0 +1,22 @@
+public class Mitsubishi extends Car {
+
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> startEngine()";
+    }
+}
+
