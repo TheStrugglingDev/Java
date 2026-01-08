ea0d8e991484c72db4fc96647863a381631fefb5
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2026-01-08 23:43:04 +0200

    Add Item class with size-based pricing and basic MealOrder structure

--- 23-OOP-burger-challenge/src/Main.java
+++ 23-OOP-burger-challenge/src/Main.java
@@ -15,6 +15,8 @@ public class Main {
 //        burger.printItem();
 
         MealOrder regularMeal = new MealOrder();
+        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+        regularMeal.setDrinkSize("LARGE");
         regularMeal.printItemizedList();
     }
 }
