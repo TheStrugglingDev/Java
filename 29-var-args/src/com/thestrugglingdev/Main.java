eed5223c3b95a89488a16d768f85e3f590780f12
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2026-01-12 22:32:04 +0200

    feat: demonstrate varargs usage with String... parameters

---
+++ 29-var-args/src/com/thestrugglingdev/Main.java
@@ -0,0 +1,31 @@
+package com.thestrugglingdev;
+
+public class Main {
+
+    public static void main(String... args) {
+
+        System.out.println("Hello World Again");
+
+        String[] splitStrings = "Hello World Again".split(" ");
+        printText(splitStrings);
+
+        System.out.println("_".repeat(20));
+        printText("Hello");
+
+        System.out.println("_".repeat(20));
+        printText("Hello", "World", "Again");
+
+        System.out.println("_".repeat(20));
+        printText();
+
+        String[] sArray = {"first", "second", "third", "four", "fifth"};
+        System.out.println(String.join(", ", sArray));
+    }
+
+    private static void printText(String... textList) {
+
+        for (String t : textList) {
+            System.out.println(t);
+        }
+    }
+}
