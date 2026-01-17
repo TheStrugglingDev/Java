9bc081d1be45d3cfa854478bdce6f359b90fdda7
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2026-01-17 20:45:12 +0200

    feat: explore List.of immutability, ArrayList operations, and generics

--- 33-arrayLists/src/com/strugglingdev/Main.java
+++ 33-arrayLists/src/com/strugglingdev/Main.java
@@ -6,7 +6,13 @@ import java.util.Arrays;
 record GroceryItem(String name, String type, int count) {
 
     public GroceryItem(String name) {
+
         this(name, "DIARY", 1);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
     }
 }
 
@@ -26,5 +32,11 @@ public class Main {
 
         ArrayList<GroceryItem> groceryList = new ArrayList<>();
         groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(new GroceryItem("oranges", "PRODUCE", 6));
+        groceryList.set(0,
+                new GroceryItem("apples", "PRODUCE", 6));
+        groceryList.remove(1);
+        System.out.println(groceryList);
     }
 }
