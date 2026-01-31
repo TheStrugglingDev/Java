9749e91796904df1bba749c8bd980b5783666b04
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2026-02-01 01:07:04 +0200

    feat: explore LinkedList operations and iteration methods

--- 36-LinkedList/src/com/strugglingdev/Main.java
+++ 36-LinkedList/src/com/strugglingdev/Main.java
@@ -22,9 +22,10 @@ public class Main {
 
 //        gettingElements(placesToVisit);
 
-        printItinerary(placesToVisit);
-        printItinerary2(placesToVisit);
-        printItinerary3(placesToVisit);
+//        printItinerary(placesToVisit);
+//        printItinerary2(placesToVisit);
+//        printItinerary3(placesToVisit);
+        testIterator(placesToVisit);
     }
 
     private static void addMoreElements(LinkedList<String> list) {
@@ -121,5 +122,23 @@ public class Main {
             previousTown = town;
         }
         System.out.println("Trip ends at " + list.getLast());
+    }
+
+    private static void testIterator(LinkedList<String> list) {
+
+        var iterator = list.listIterator();
+        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+            if (iterator.next().equals("Brisbane")) {
+                iterator.add("Lake Wivenhoe");
+            }
+        }
+        while (iterator.hasPrevious()) {
+            System.out.println(iterator.previous());
+        }
+        System.out.println(list);
+
+        var iterator2 = list.listIterator(3);
+        System.out.println(iterator2.previous());
     }
 }
