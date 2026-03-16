7b5ef5a98b6b4685ac05964257b57f70533d3cb0
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2026-03-16 03:16:36 +0200

    feat: add Bird class implementing Animal, FlightEnabled, and Trackable

---
+++ 44-interfaces/src/com/strugglingdev/Animal.java
@@ -0,0 +1,21 @@
+package com.strugglingdev;
+
+interface FlightEnabled {
+
+    void takeOff();
+
+    void land();
+
+    void fly();
+}
+
+interface Trackable {
+
+    void track();
+}
+
+
+public abstract class Animal {
+
+    public abstract void move();
+}
