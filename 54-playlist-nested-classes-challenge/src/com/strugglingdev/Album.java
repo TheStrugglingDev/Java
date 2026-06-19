57e75dd712ed48a508f2fb87e194036371ec2b13
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2026-06-19 19:14:37 +0300

    feat: complete playlist nested classes challenge

---
+++ 54-playlist-nested-classes-challenge/src/com/strugglingdev/Album.java
@@ -0,0 +1,83 @@
+package com.strugglingdev;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+
+
+    private String name;
+    private String artist;
+    private SongList songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new SongList();   // IMPORTANT
+    }
+
+    public boolean addSong(String title, double duration) {
+        return songs.add(new Song(title, duration));
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        Song song = songs.findSong(trackNumber);
+
+        if (song == null) {
+            System.out.println("This album does not have a track " + trackNumber);
+            return false;
+        }
+
+        playList.add(song);
+        return true;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song song = songs.findSong(title);
+
+        if (song == null) {
+            System.out.println("The song " + title + " is not in this album");
+            return false;
+        }
+
+        playList.add(song);
+        return true;
+    }
+
+    public static class SongList {
+
+        private ArrayList<Song> songs;
+
+        private SongList() {
+            this.songs = new ArrayList<>();
+        }
+
+        private boolean add(Song song) {
+            if (findSong(song.getTitle()) != null) {
+                return false;
+            }
+
+            songs.add(song);
+            return true;
+        }
+
+        private Song findSong(String title) {
+            for (Song song : songs) {
+                if (song.getTitle().equals(title)) {
+                    return song;
+                }
+            }
+            return null;
+        }
+
+        private Song findSong(int trackNumber) {
+            int index = trackNumber - 1;
+
+            if (index >= 0 && index < songs.size()) {
+                return songs.get(index);
+            }
+
+            return null;
+        }
+    }
+}
