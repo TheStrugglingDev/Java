d94bbd153b89d6a7a5d392eeb4bd7b9cc3de24f4
N1ckPed <134404592+N1ckPed@users.noreply.github.com>
2026-08-28 22:17:53 +0300

    feat: practice built-in functional interfaces with lambdas

--- 57-Lambda-expressions/src/com/strugglingdev/Main.java
+++ 57-Lambda-expressions/src/com/strugglingdev/Main.java
@@ -3,6 +3,7 @@ package com.strugglingdev;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 
 public class Main {
@@ -38,6 +39,12 @@ public class Main {
                 new double[]{35.1556, -90.0659});
 
         coords.forEach(s -> System.out.println(Arrays.toString(s)));
+
+        BiConsumer<Double, Double> p1 = (lat, lng) ->
+                System.out.printf("[lat:%.3f lon:%.3f]%n", lat, lng);
+
+        var firstPoint = coords.get(0);
+        processPoint(firstPoint[0], firstPoint[1], p1);
     }
 
     public static <T> T calculator(BinaryOperator<T> function, T value1, T value2) {
@@ -45,5 +52,9 @@ public class Main {
         T result = function.apply(value1, value2);
         System.out.println("Result of operation is: " + result);
         return result;
+    }
+
+    public static <T> void processPoint(T t1, T t2, BiConsumer<T, T> consumer) {
+        consumer.accept(t1, t2);
     }
 }
